a49e922252794e1b6d013568a4ef88d26d1178cf
TMagdalena <46978091+TMagdalena@users.noreply.github.com>
2022-01-04 17:24:26 +0100

    Update make_predict_azure_app.sh

--- flask-sklearn/make_predict_azure_app.sh
+++ flask-sklearn/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://myappservice2.azurewebsites.net:$PORT/predict 
+     -X POST https://myappservice6.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
