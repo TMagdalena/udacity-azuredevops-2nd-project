36735047e6bc706eff7733873a7fa7ad156327c9
TMagdalena <46978091+TMagdalena@users.noreply.github.com>
2022-01-06 15:16:20 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,28 +3,27 @@ from locust import HttpUser, task
 class MyTaskSet(HttpUser):
   @task
   def index(self):
-    self.client.get("/posts")
+    self.client.get('/')
     
   @task
   def users(self):
-    self.client.post("/posts", json=
-        {
-        "CHAS":{
-            "0":0
-        },
-        "RM":{
-            "0":6.575
-        },
-        "TAX":{
-            "0":296.0
-        },
-        "PTRATIO":{
-            "0":15.3
-        },
-        "B":{
-            "0":396.9
-        },
-        "LSTAT":{
-            "0":4.98
-        }
-        })
+    self.client.post("/posts", json={
+    "CHAS":{
+        "0":0
+    },
+    "RM":{
+        "0":6.575
+    },
+    "TAX":{
+        "0":296.0
+    },
+    "PTRATIO":{
+        "0":15.3
+    },
+    "B":{
+        "0":396.9
+    },
+    "LSTAT":{
+        "0":4.98
+    }
+    })
